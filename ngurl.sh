47740246a1413a97e16517948b37cb26b5ea44ad
EC2 Default User <ec2-user@ip-172-31-43-161.ap-northeast-1.compute.internal>
2019-01-27 15:42:32 +0000

    ngrok

--- ngurl.sh
+++ ngurl.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+echo ""
+echo "================="
+echo ""
 curl -s "localhost:52088/api/tunnels" | awk -F',' '{print $3}' | awk -F'"' '{print $4}' | awk -F'//' '{print $2}'
+echo ""
+echo "================="
+echo ""
