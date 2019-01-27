dd1929984e84fb7cc35afa37bc6d292d259c1377
EC2 Default User <ec2-user@ip-172-31-43-161.ap-northeast-1.compute.internal>
2019-01-27 15:07:49 +0000

    docker-compose

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 set -ex
 # SET THE FOLLOWING VARIABLES
 # docker hub username
-USERNAME=uuboyscy
+USERNAME="uuboyscy"
 # image name
-IMAGE=line_chatbot_jupyter
+IMAGE="line_chatbot_jupyter"
 docker build -t $USERNAME/$IMAGE:latest .
 version=`cat VERSION`
 echo "version: $version"
--- dockerfile
+++ dockerfile
@@ -1,3 +1,3 @@
 FROM jupyter/base-notebook
 RUN pip install Flask==0.12 requests line-bot-sdk
-RUN pip install redis MySQL-python
+RUN pip install redis PyMySQL
