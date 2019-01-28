5ca4853873ef13fd288d16ca4e9745351ba1536d
EC2 Default User <ec2-user@ip-172-31-36-100.ap-northeast-1.compute.internal>
2019-01-28 09:37:17 +0000

    docker compose link jupyter-flask-ngrok-redis

--- ngurl.sh
+++ ngurl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo ""
-echo "================="
+echo "======Ngrok======"
 echo ""
-curl -s "localhost:52088/api/tunnels" | awk -F',' '{print $3}' | awk -F'"' '{print $4}' | awk -F'//' '{print $2}'
+curl -s "localhost:54088/api/tunnels" | awk -F',' '{print $3}' | awk -F'"' '{print $4}' | awk -F'//' '{print $2}'
 echo ""
 echo "================="
 echo ""
